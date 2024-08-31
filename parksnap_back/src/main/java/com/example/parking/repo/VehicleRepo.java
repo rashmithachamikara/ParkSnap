9b5be611dd0345a90657a44dc36794ca969a42f4
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-09-01 02:36:36 +0530

    Added getVehiclesByUserId endpoint. Fixed minor issue in Slot naming

--- parksnap_back/src/main/java/com/example/parking/repo/VehicleRepo.java
+++ parksnap_back/src/main/java/com/example/parking/repo/VehicleRepo.java
@@ -1,8 +1,13 @@
 package com.example.parking.repo;
 
-import com.example.parking.entity.Lot;
 import com.example.parking.entity.Vehicle;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface VehicleRepo extends JpaRepository<Vehicle, String> {
+    @Query("SELECT v FROM Vehicle v WHERE v.user.userId = :userId")
+    List<Vehicle> getVehiclesByUserId(@Param("userId") int userId);
 }
--- parksnap_back/src/main/java/com/example/parking/service/VehicleService.java
+++ parksnap_back/src/main/java/com/example/parking/service/VehicleService.java
@@ -1,4 +1,36 @@
 package com.example.parking.service;
 
+import com.example.parking.dto.UserDTO;
+import com.example.parking.dto.VehicleDTO;
+import com.example.parking.entity.Vehicle;
+import com.example.parking.repo.UserRepo;
+import com.example.parking.repo.VehicleRepo;
+import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional
 public class VehicleService {
+    @Autowired
+    private VehicleRepo vehicleRepo;
+
+    @Autowired
+    private UserRepo userRepo;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    public List<VehicleDTO> getVehiclesByUserId(int userId) {
+        if (userRepo.existsById(userId)) {
+            List<Vehicle> vehicleList = vehicleRepo.getVehiclesByUserId(userId);
+            return modelMapper.map(vehicleList, new TypeToken<List<VehicleDTO>>() {}.getType());
+        } else {
+            return null;
+        }
+    }
 }
