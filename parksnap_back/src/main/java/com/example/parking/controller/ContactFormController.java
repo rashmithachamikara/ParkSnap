02705819a73e82d785eeec43adb89d88bd889484
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-09-18 23:33:26 +0530

    Added getAllcontactForms endpoint

--- parksnap_back/src/main/java/com/example/parking/controller/ContactFormController.java
+++ parksnap_back/src/main/java/com/example/parking/controller/ContactFormController.java
@@ -7,10 +7,9 @@ import com.example.parking.util.VarList;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/contactForm")
@@ -25,8 +24,8 @@ public class ContactFormController {
 
     @PostMapping(value = "/saveContactForm")
     public ResponseEntity saveContactForm(@RequestBody ContactFormDTO contactformDTO) {
-        try{
-            String res=contactFormService.saveContactForm(contactformDTO);
+        try {
+            String res = contactFormService.saveContactForm(contactformDTO);
             if (res.equals("00")) {
                 responseDTO.setCode(VarList.RSP_SUCCESS);
                 responseDTO.setMessage("Success!");
@@ -47,5 +46,22 @@ public class ContactFormController {
             return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
 
         }
-}
+    }
+
+    @GetMapping("/getAllcontactForms")
+    public ResponseEntity getAllContactForms() {
+        try {
+            List<ContactFormDTO> contactFormDTOList = contactFormService.getAllContactForms();
+            responseDTO.setCode(VarList.RSP_SUCCESS);
+            responseDTO.setMessage("Success!");
+            responseDTO.setContent(contactFormDTOList);
+            return new ResponseEntity(responseDTO, HttpStatus.ACCEPTED);
+
+        } catch (Exception ex) {
+            responseDTO.setCode(VarList.RSP_ERROR);
+            responseDTO.setMessage(ex.getMessage());
+            responseDTO.setContent(null);
+            return new ResponseEntity(responseDTO, HttpStatus.INTERNAL_SERVER_ERROR);
+        }
+    }
 }
--- parksnap_back/src/main/java/com/example/parking/service/ContactFormService.java
+++ parksnap_back/src/main/java/com/example/parking/service/ContactFormService.java
@@ -6,8 +6,12 @@ import com.example.parking.repo.ContactFormRepo;
 import com.example.parking.util.VarList;
 import jakarta.transaction.Transactional;
 import org.modelmapper.ModelMapper;
+import org.modelmapper.TypeToken;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Service
 @Transactional
@@ -18,14 +22,20 @@ public class ContactFormService {
     @Autowired
     private ModelMapper modelMapper;
 
-        public String saveContactForm(ContactFormDTO contactformDTO) {
-            if (contactformRepo.existsById(contactformDTO.getUserCode())) {
-                return VarList.RSP_DUPLICATED;
-            } else {
-                contactformRepo.save(modelMapper.map(contactformDTO, ContactForm.class));
-                return VarList.RSP_SUCCESS;
-            }
+    public String saveContactForm(ContactFormDTO contactformDTO) {
+        if (contactformRepo.existsById(contactformDTO.getUserCode())) {
+            return VarList.RSP_DUPLICATED;
+        } else {
+            contactformRepo.save(modelMapper.map(contactformDTO, ContactForm.class));
+            return VarList.RSP_SUCCESS;
         }
+    }
+
+    public List<ContactFormDTO> getAllContactForms() {
+        List<ContactForm> contactFormList = contactformRepo.findAll();
+        return modelMapper.map(contactFormList, new TypeToken<ArrayList<ContactFormDTO>>() {
+        }.getType());
+    }
 }
 
 
