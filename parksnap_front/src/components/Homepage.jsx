611557feaded978532455a50927596c8a92f03c7
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-09-25 13:17:55 +0530

    Merged Start page to Homepage

--- parksnap_front/src/components/Homepage.jsx
+++ parksnap_front/src/components/Homepage.jsx
@@ -1,7 +1,7 @@
 // src/HomePage.js
 import React from 'react';
-import { Button } from 'react-bootstrap';
 import './Homepage.css';
+import logo from '../assets/parksnap_logo.png'; // Make sure to place parksnap.png in the src folder
 
 const Homepage = () => {
   const handleRegister = () => {
@@ -14,23 +14,19 @@ const Homepage = () => {
 
   return (
     <div className="homepage">
-      <div className="container text-center d-flex flex-column justify-content-center align-items-center">
-        <div className="logo">
-          <span className="logo-line"></span>
-        </div>
-        <h1 className="title">PARKSNAP</h1>
-        <p className="subtitle">Start Your Day with Easy Parking</p>
+      <div className="container">
+        <img src={logo} alt="ParkSnap logo" className="logo" />
+        <h1>Start Your Day With Easy Parking</h1>
         <div className="button-container">
-          <Button className="register-btn" onClick={handleRegister}>
-            Register
-          </Button>
-          <Button className="signin-btn" onClick={handleSignIn}>
-            Sign in
-          </Button>
+          <button className="login-button" onClick={handleSignIn}>Login</button>
+          <button className="register-button" onClick={handleRegister}>Register</button>
         </div>
+        <footer>© 2024 ParkSnap. Designed By TechTuners.</footer>
       </div>
     </div>
   );
 };
 
 export default Homepage;
+
+
