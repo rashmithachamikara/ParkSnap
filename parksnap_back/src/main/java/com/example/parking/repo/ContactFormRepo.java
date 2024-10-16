b397d395ede0fb66dbff3fbaeab377a352b634a4
rashmithachamikara <rashmitha.chamickara@gmail.com>
2024-10-17 01:25:28 +0530

    Resolve author mismatch

--- parksnap_back/src/main/java/com/example/parking/repo/ContactFormRepo.java
+++
@@ -1,7 +0,0 @@
-package com.example.parking.repo;
-
-import com.example.parking.entity.ContactForm;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ContactFormRepo extends JpaRepository<ContactForm, Integer> {
-}
